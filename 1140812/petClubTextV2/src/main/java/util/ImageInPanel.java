897fdcdd31153b7195816c807716681304afbca9
devilsunmer <a0972118545@gmail.com>
2025-08-12 15:37:52 +0800

    updateV3

--- 1140812/petClubTextV2/src/main/java/util/ImageInPanel.java
+++ 1140812/petClubTextV2/src/main/java/util/ImageInPanel.java
@@ -2,24 +2,31 @@ package util;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class ImageInPanel extends JPanel {
 
-    private Image image;
+	private Image image;
 
-    public ImageInPanel(String imagePath) {
-        // 加載圖片
-        image = new ImageIcon(getClass().getResource(imagePath)).getImage();
-    }
+	public ImageInPanel(String imagePath) {
+		URL url = getClass().getResource(imagePath);
+		if (url == null) {
+			System.out.println("圖片載入失敗！");
+			image = null; // 或做其他錯誤處理
+		} else {
+			System.out.println("圖片載入成功！路徑：" + url);
+			image = new ImageIcon(url).getImage();
+		}
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
-        // 繪製圖片到面板，這裡會將圖片繪製到 (0, 0) 位置
-        g.drawImage(image, 0, 0, this);  // 繪製圖片，this 是 ImageObserver
-    }
+		// 繪製圖片到面板，這裡會將圖片繪製到 (0, 0) 位置
+		g.drawImage(image, 0, 0, this); // 繪製圖片，this 是 ImageObserver
+	}
 }
